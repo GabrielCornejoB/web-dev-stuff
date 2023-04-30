b35003101efef29b0f79245d49da0fea07abdf94
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-04-30 11:30:10 -0500

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import styles from "./App.module.scss";
+
 function App() {
-  return <p>Hello world</p>;
+  return (
+    <div className={styles.panel}>
+      <div className={styles.title}>
+        <h1>WebDevStuff</h1>
+      </div>
+      <nav>nav</nav>
+      <div className={styles.searchbar}>search</div>
+      <main>main</main>
+    </div>
+  );
 }
 
 export default App;
