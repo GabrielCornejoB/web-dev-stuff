6a7f6cb5d213f159840ded5d464bbc226862325f
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-05-01 11:13:20 -0500

    Searchbar section

--- src/features/ui/Searchbar/Searchbar.tsx
+++ src/features/ui/Searchbar/Searchbar.tsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
+import { IconContext } from "react-icons";
+import { MdDarkMode, MdLightMode } from "react-icons/md";
+import { BiSearch } from "react-icons/bi";
 import s from "./Searchbar.module.scss";
 
 function Searchbar() {
-  return <div className={s.searchbar}>Searchbar</div>;
+  const [isDarkMode, setIsDarkMode] = useState(true);
+  return (
+    <div className={s.searchbar}>
+      <IconContext.Provider value={{ size: "25px" }}>
+        <div className={s.searchInput}>
+          <BiSearch className={s.searchIcon} />
+          <input />
+        </div>
+
+        <div
+          className={s.toggleTheme}
+          onClick={() => setIsDarkMode(!isDarkMode)}
+        >
+          {isDarkMode ? <MdDarkMode /> : <MdLightMode />}
+          <span>{isDarkMode ? "Dark Theme" : "Light Theme"}</span>
+        </div>
+      </IconContext.Provider>
+    </div>
+  );
 }
 export default Searchbar;
