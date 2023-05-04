3e862f81edc6e8a4273fe705f1a557cd11840b37
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-05-04 17:32:13 -0500

    Fixed text jump while image is loading

--- src/features/main/Card/Card.tsx
+++ src/features/main/Card/Card.tsx
@@ -9,7 +9,7 @@ interface Props {
 function Card({ name, url, imageUrl }: Props) {
   return (
     <a href={url} target="_blank" className={`${s.cardContainer} ${s.card}`}>
-      <img src={imageUrl} alt={name} />
+      <img src={imageUrl} alt={name} width="50%" height="50%" />
       <h3>{name}</h3>
     </a>
   );
